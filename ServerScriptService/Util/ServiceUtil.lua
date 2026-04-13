70ecccee9d10b0a9f21eab33418917482bec1e86
Nekthegeek <Unecquiahill96@gmail.com>
2026-04-13 07:51:15 -0400

    Create ServiceUtil.lua with service initialization utilities

---
+++ ServerScriptService/Util/ServiceUtil.lua
@@ -0,0 +1,11 @@
+-- ServiceUtil.lua
+
+local ServiceUtil = {}
+
+function ServiceUtil:InitializeServices()
+    -- Initialize your services here
+    -- Example:
+    -- self.MyService = game:GetService('MyService')
+end
+
+return ServiceUtil
